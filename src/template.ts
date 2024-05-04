61cc0622533113ed5a51c6ad2fb1f0a0cbc13151
Jiří Špác <capajj@gmail.com>
2024-05-04 11:45:05 +0200

    drop handlebars import hack

--- src/template.ts
+++ src/template.ts
@@ -8,13 +8,8 @@ import { handlebarsDateHelper, operatorHelpers } from "./handlebars-helpers"
 import { JSONValue } from "./jsonType"
 import { ContentArray } from "./schemas"
 
-// @ts-expect-error handlebarsEvalles sometimes has issues with CJS/ESM interop so we need to use the default export
-const handlebars: typeof handlebarsType = handlebarsEvalles.registerHelper
-  ? handlebarsEvalles
-  : handlebarsEvalles.default
-
-handlebars.registerHelper("$date", handlebarsDateHelper)
-handlebars.registerHelper(operatorHelpers)
+handlebarsEvalles.registerHelper("$date", handlebarsDateHelper)
+handlebarsEvalles.registerHelper(operatorHelpers)
 const Visitor = vanillaHandlebars.Visitor
 
 /*
@@ -56,7 +51,9 @@ export const compileStringHandlebars = (
       .replace(/\n?({{else}})\n?/g, "$1") // Remove newline before and after {{else}}
       .replace(/\n({{\/(if|unless|with)}})/g, "$1") // Remove newline before closing block helpers
 
-    const template = handlebars.compileAST(preprocessedText, { noEscape: true }) // regular compile cannot be used in cloudflare worker
+    const template = handlebarsEvalles.compileAST(preprocessedText, {
+      noEscape: true,
+    }) // regular compile cannot be used in cloudflare worker
 
     const parsedInput: Record<string, JSONValue | TemplateObject> = {}
     for (const key in input) {
@@ -210,9 +207,8 @@ class VariableScanner extends Visitor {
 
 export function extractVariablesForHandlebars(template: string): string[] {
   try {
-    const ast = handlebars.parse(template)
-    const scanner = new VariableScanner() as handlebarsEvalles.VariableScanner
-
+    const ast = handlebarsEvalles.parse(template)
+    const scanner = new VariableScanner()
     scanner.accept(ast)
     return scanner.variables
   } catch (error) {
