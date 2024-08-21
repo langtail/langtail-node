5e4ab00f6379af3b5a140c170fa2fa5c3b3f38ea
Vojtěch Tranta <vojta.tranta@gmail.com>
2024-08-21 15:58:27 +0200

    re-export Stream as helper function from SDK

--- src/stream/index.ts
+++ src/stream/index.ts
@@ -1,5 +1,6 @@
 import { ChatCompletionStream } from 'openai/lib/ChatCompletionStream';
 import { AssistantStream } from 'openai/lib/AssistantStream';
+import { Stream } from "openai/streaming"
 
 
 export const chatStreamToRunner = (stream: ReadableStream<any>): ChatCompletionStream => {
@@ -9,7 +10,12 @@ export const assistantStreamToRunner = (stream: ReadableStream<any>): AssistantS
   return AssistantStream.fromReadableStream(stream);
 }
 
+export const readableStreamFromSSEResponse = (reponse: Response, controller?: AbortController): ReadableStream<any> => {
+  return Stream.fromSSEResponse(reponse, controller || new AbortController()).toReadableStream();
+}
+
 export type {
   ChatCompletionStream,
-  AssistantStream
+  AssistantStream,
+  Stream
 }
